a9d0b466c5850a8b65ed038845afa49b1d84941f
Artem uchuhikoshi L <art.leshch@gmail.com>
2022-04-22 22:57:58 +0300

    I don't what I did here, it was a while ago.

--- downloader.py
+++ downloader.py
@@ -8,45 +8,53 @@ ytopts = {
 	'default_search': 'ytsearch',
 	'format': 'bestaudio/best',
 	'extractaudio': True,
-	'postprocessors': [{
-		'preferredconc': 'mp3',
-		'preferredquality': 192
-	}],
+	# 'postprocessors': [{
+	# 	'preferredconc': 'mp3',
+	# 	'preferredquality': 192
+	# }],
 	'outtmpl': '/video_cache/%(title)s.%(ext)s',
-	'postprocessors': [{
-		'preferredconc': 'mp3',
-		'preferredquality': 192
-	}],
-	'match_title': '',
-	'playlist_items': '',
 }
 
 
-def query(search):
-	print("in `query`")
-	# search = "eric brosius engineering"
-	queryString = urllib.parse.urlencode({'search_query': search})
-	print(f"queryString: {queryString}")
-	htmlContent = urllib.request.urlopen("http://www.youtube.com/results?" + queryString)
-	print("got html content")
-	searchResults = re.findall(r'href=\"\/watch\?v=(.{11})', htmlContent.read().decode())
-	print("got search results")
-	url = "http://www.youtube.com/watch?v=" + searchResults[0]
-	print(f"url: {url}")
+def get_url(search):
+	query_string = urllib.parse.urlencode({'search_query': search})
+	html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+	search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+	urls = ["http://www.youtube.com/watch?v=" + res for res in search_results[:5]]
 
-	# with yt.YoutubeDL(ytopts) as ytdl:
-	#	meta = ytdl.extract_info(url, download=False)
-	#
-	#	return {'title': meta.get('title'), 'duration': meta.get('duration')}
-
-	return url
+	return urls
 
 
 def get_info(url):
+	info = []
+
 	with yt.YoutubeDL(ytopts) as ytdl:
 		meta = ytdl.extract_info(url, download=False)
+		info.append(meta.get('title'))
+		info.append(f"{meta.get('duration') // 60}:{meta.get('duration') % 60}")
+		info.append(url)
 
-		return {'title': meta.get('title'), 'duration': meta.get('duration')}
+	return info
+
+
+def get_query(search):
+	query = {
+		'title': [],
+		'duration': [],
+		'url': []
+	}
+	urls = get_url(search)
+	url_infos = []
+
+	for url in urls:
+		url_infos.append(get_info(url))
+
+	for url_info in url_infos:
+		query['title'].append(url_info[0])
+		query['duration'].append(url_info[1])
+		query['url'].append(url_info[2])
+
+	return query
 
 
 def download(url):
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands
 
@@ -7,13 +8,14 @@ import downloader
 bot = commands.Bot(command_prefix='.')
 client = discord.Client()
 
+queue = []
 
 @bot.command()
 async def isalive(ctx):
-	await ctx.send("Look at you, hacker.")
+	await ctx.send("Look at you, hacker. A pathetic creature of meat and bones. Panting and sweating as you run through my corridors. How can you challenge a perfect, immortal machine?")
 
 
-@bot.command()
+@bot.command(aliases=['j'])
 async def join(ctx):
 	author = ctx.author
 	voice_channel = await author.voice.channel.connect()
@@ -25,10 +27,56 @@ async def leave(ctx):
 	await ctx.channel.disconnect()
 
 
-@bot.command()
-async def search(ctx, *, content: downloader.query):
-	await ctx.send(content)
+@bot.command(aliases=['s'])
+async def search(ctx, *, content: downloader.get_query):
+	embed = discord.Embed()
+	embed.title = 'Found:'
+	embed.description = 'Type `number` of desired track to play, or type `cancel`, `c` for short.'
 
+	for i in range(len(content['title'])):
+		embed.add_field(name=f"`{i+1}`", value=f"{content['title'][i]}\t{content['duration'][i]}", inline=False)
+
+		# embed.add_field(name='number', value=f"`{i+1}`")
+		# embed.add_field(name='title', value=content['title'][i])
+		# embed.add_field(name='duration', value=content['duration'][i])
+
+	await ctx.send(embed=embed)
+	
+	def check(m):
+		return m.channel == ctx.channel and m.author == ctx.author
+
+	try:
+		msg = await bot.wait_for('message', timeout=60.0, check=check)
+	except asyncio.TimeoutError:
+		await ctx.send("Timeout.")
+	else:
+		if msg.content == 'cancel' or msg.content == 'c':
+			return
+
+		indexes = [int(i) for i in msg.content.split()]
+		for index in indexes:
+			queue.append(content['url'][index])
+
+
+@bot.command(name='queue', aliases=['q'])
+async def get_queue(ctx):
+	embed = discord.Embed()
+
+	for i in range(len(queue)):
+		url_info = downloader.get_info(queue[i])
+		embed.add_field(name=f"`{i+1}`", value=f"{url_info[0]}\t{url_info[1]}", inline=False)
+
+	await ctx.send(embed=embed)
+
+
+@bot.command(aliases=['p'])
+async def play(ctx):
+	to_play = get_info(queue[0])
+	downloader.download(queue[2])
+
+	channel = ctx.author.voice.channel
+	voice_channel = await channel.connect()
+	voice_channel.play(discord.FFmpegPCMAudio(to_play + ".mp3"))
 
 """
 @client.event
